2292e8d4fc4102738ff7f7936d3bd530308337b7
guzhexuan <2974127659@qq.com>
2023-07-19 16:23:40 +0800

    use Config file to simplify dataset I/O operations

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 from loadata import Dataset
 from train import train
 from baselines.st_gcn_baseline import ST_GCN
-import argparse
+from baselines.TCN_baseline import TCN
+from config import Config
 
 if __name__ == "__main__":
-    dataset = Dataset()
+    cfg = Config()
+
+    dataset = Dataset(cfg)
+
+    # model = TCN(t_kernel=9, time_steps=dataset.maxFrame, num_classes=3)
+
     # model = ST_GCN(3, 3)
-    # train(model, dataset)
+
+    # train(model, dataset, item=cfg.item, epochs=40, baseline="TCN")
