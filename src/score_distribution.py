de3f1224acfcff83fe4c6290eb19ffefdbe12257
guzhexuan <2974127659@qq.com>
2023-07-19 16:13:51 +0800

    check the imbalanced problem

--- src/score_distribution.py
+++ src/score_distribution.py
@@ -2,7 +2,7 @@ from config import Config
 import matplotlib.pyplot as plt
 import numpy as np
 
-#### For Dr.Patrick Kwong
+# For Dr.Patrick Kwong
 
 """
 图片显示后 自行放大到全图
@@ -11,7 +11,7 @@ import numpy as np
 
 if __name__ == "__main__":
     cfg = Config()
-    labels = cfg.labels             # Dataframe
+    labels = cfg.labels  # Dataframe
 
     fig = plt.figure()
     fig.suptitle("score distribution of each item")
@@ -28,10 +28,7 @@ if __name__ == "__main__":
                     count += 1
             counts.append(count)
         plt.subplot(4, 4, i + 1)
-        plt.title(cfg.label_dict["item" + str(i + 1)], font={'family':'Arial', 'size':8})
+        plt.title(cfg.label_dict["item" + str(i + 1)], font={'family': 'Arial', 'size': 8})
         plt.bar(x_labels, counts, width=0.5)
 
-
-
     plt.show()
-
