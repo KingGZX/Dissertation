a072e04961e2383c5f12245329e136ba16b6cbea
guzhexuan <2974127659@qq.com>
2023-07-28 10:58:29 +0800

    multihead graph attention, which just enlarge the output channels n (head num) times

--- src/utils/graph_attention_block.py
+++ src/utils/graph_attention_block.py
@@ -4,13 +4,18 @@ import torch.nn.functional as F
 
 
 class GAT_Block(nn.Module):
-    def __init__(self, in_channels=3, hidden_dim=16, dropout=0.3):
+    """
+    Graph Attention Block, including multi-head attention
+    """
+    def __init__(self, n_heads=1, in_channels=3, hidden_dim=16, dropout=0.3):
         super(GAT_Block, self).__init__()
         self.in_channels = in_channels
         self.hidden_dim = hidden_dim
-        self.linear = nn.Linear(self.in_channels, self.hidden_dim)
-        self.weight = nn.Linear(2 * self.hidden_dim, 1)
+        self.heads = n_heads
+        self.linear1 = nn.Linear(self.in_channels, self.hidden_dim * n_heads)
+        self.linear2 = nn.Linear(2 * self.hidden_dim, 1)
         self.dropout = nn.Dropout(dropout)
+        self.LRelu = nn.LeakyReLU()
 
     def forward(self, x, mask):
         """
@@ -21,45 +26,43 @@ class GAT_Block(nn.Module):
                 --------but for graph partition, have no idea now ---------
         :return:
         """
-        x = torch.permute(x, [0, 2, 3, 1])         # [batch, frames, nodes, channel]
+        x = torch.permute(x, [0, 2, 3, 1])  # [batch, frames, nodes, channel]
         batch, frames, nodes, channels = x.shape
 
-        x = self.linear(x)
+        x = self.linear1(x)                 # [batch, frames, nodes, hidden_dim * heads]
+
+        x = x.view(batch, self.heads, frames, nodes, self.hidden_dim)
 
         x1 = torch.unsqueeze(x, dim=-2)
         x2 = torch.unsqueeze(x, dim=-3)
 
-        x1 = x1.repeat(1, 1, 1, nodes, 1)
-        x2 = x2.repeat(1, 1, nodes, 1, 1)
+        x1 = x1.repeat(1, 1, 1, 1, nodes, 1)
+        x2 = x2.repeat(1, 1, 1, nodes, 1, 1)
 
         x3 = torch.concat([x1, x2], dim=-1)
 
-        x4 = self.weight(x3)
+        x4 = self.LRelu(self.linear2(x3))
+        # [batch, heads, frames, nodes, nodes], the feature map stands for the contribution of Node i to Node j
         x4 = torch.squeeze(x4, dim=-1)
 
         x4 = F.softmax(x4, dim=-1)
         x4 = torch.multiply(x4, mask)
-        # apply adjacency matrix as the mask to each attention matrix.Element-wise multiplication to eliminate invalid..
+        # apply adjacency matrix as the mask to each attention matrix.
+        # Element-wise multiplication to eliminate invalid contribution of neighbor node
 
-        out = torch.einsum("bfnn, bfnj -> bfnj", x4, x)
+        out = torch.einsum("bhfnn, bhfnj -> bhfnj", x4, x)      # [batch, head, frame, nodes, hidden_dim]
+
+        out = out.view(batch, frames, nodes, -1)                # [batch, frames, nodes, hidden_dim * heads]
 
         out = torch.permute(out, [0, 3, 1, 2])  # back to [batch, channel, frame, joints]
         return out
 
 
-class GMAT_Block(nn.Module):
-    """
-    It's graph multi-head attention block
-    """
-    def __init__(self):
-        super(GMAT_Block, self).__init__()
-        pass
-
-
 # code for debugging
 """
-instance = GAT_Block()
+instance = GAT_Block(n_heads=3)
 x = torch.randn(2, 3, 200, 19)
-out = instance(x)
+mask = torch.ones((19, 19))
+out = instance(x, mask)
 print(out.shape)
 """
